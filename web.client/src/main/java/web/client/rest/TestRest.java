849379e581a688dcd9a78141e87aa4861dc35e71
ivan.mamai00@mail.ru <250219As>
2022-01-28 14:40:41 +0300

    done

--- web.client/src/main/java/web/client/rest/TestRest.java
+++ web.client/src/main/java/web/client/rest/TestRest.java
@@ -1,15 +1,11 @@
 package web.client.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import web.client.model.AuthReq;
 import web.client.model.AuthResp;
@@ -17,11 +13,13 @@ import web.client.model.User;
 
 @Configuration
 @RestController
+@RequiredArgsConstructor
 public class TestRest {
-    private WebClient webClient = WebClient.create("http://localhost:8081/");
+    private final WebClient webClient;
 
     @GetMapping("/login")
-    public Mono<AuthResp> log(@RequestBody AuthReq authReq) {
+    public Mono<AuthResp> log(@RequestBody(required = false)
+                                      AuthReq authReq) {
         return webClient
                 .post().uri("/login")
                 .body(Mono.just(new User(authReq.getUsername(), authReq.getPassword())), User.class)
