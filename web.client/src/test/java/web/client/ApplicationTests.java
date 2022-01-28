69f79d2e0e45a659b9c598456878be0a8d214894
ivan.mamai00@mail.ru <250219As>
2022-01-28 15:25:47 +0300

    delete

--- web.client/src/test/java/web/client/ApplicationTests.java
+++ web.client/src/test/java/web/client/ApplicationTests.java
@@ -19,28 +19,4 @@ import java.util.stream.Collectors;
 
 @SpringBootTest
 class ApplicationTests {
-
-    private WebClient webClient = WebClient.create("http://localhost:8081/");
-    private WebServer webServer = new WebServer(webClient);
-
-    @Test
-    void contextLoads() {
-        System.out.println(webServer.listBeer().blockFirst());
-    }
-
-    @Test
-    public void auth() throws InterruptedException {
-        System.out.println(webClient
-                .post().uri("/login")
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .body(Mono.just(new User("user", "user")), User.class)
-                .retrieve());
-    }
-
-    @Test
-    public void test(){
-//        new BC
-    }
-
-
 }
